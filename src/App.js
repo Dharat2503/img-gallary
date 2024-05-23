c2f04eb0464777bd01a153e5e9b8ebbb6e20027e
dharathakkarz <dharat@zignuts.com>
2024-05-23 14:10:54 +0530

    effect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from "./pages/Home";
+import Home2 from "./pages/Home2";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
    <Router>
     <Routes>
       <Route path="/" element={<Home/>} />
+      <Route path="/home2" element={<Home2/>} />
     </Routes>
    </Router>
    
