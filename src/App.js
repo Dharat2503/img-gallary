ebe1736e7048599227add7759fcc317148ee1606
dharathakkarz <dharat@zignuts.com>
2024-05-24 12:21:44 +0530

    center

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from "./pages/Home";
 import Home2 from "./pages/Home2";
+import Home3 from "./pages/Home3";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/home2" element={<Home2/>} />
+      <Route path="/home3" element={<Home3/>} />
     </Routes>
    </Router>
    
---
+++ src/pages/Home3.js
@@ -0,0 +1,88 @@
+
+import React from 'react';
+import '../pages/Home.css';
+
+const services = [
+  {
+    name: "Retail, E-Commerce",
+    image: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    isOverlay: true 
+  },
+  {
+    name: "Food & Restaurants",
+    image: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    isOverlay: true 
+  },
+  {
+    name: "Healthcare & Fitness",
+    image: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    isOverlay: true 
+  },
+  {
+    name: "Logistic & Distribution",
+    image: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    isOverlay: true 
+  },
+  {
+    name: "Education",
+    image: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    isOverlay: true 
+  },
+  {
+    name: "Social Networking",
+    image: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    isOverlay: true 
+  },
+  {
+    name: "Real Estate",
+    image: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    isOverlay: true 
+  },
+  {
+    name: "Travel & Hospitality",
+    image: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    isOverlay: true 
+  },
+  {
+    name: "On-Demand Solution",
+    image: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    isOverlay: true 
+  },
+  {
+    name: "Gaming",
+    image: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    isOverlay: true 
+  },
+
+];
+
+const Home3 = () => {
+  return (
+    <section className="service_section" id="service_section">
+      <div className="container">
+        <div className="row">
+          <div className="service_area">
+            <h4>Our Sectors Of Service</h4>
+            <p>
+              We Create Applications In Practically Every Category Here. We’ll Build
+              Whatever You Can Imagine.
+            </p>
+            <div className="services_grid">
+              {services.map((service, index) => (
+                <div
+                  key={index}
+                  className={`service_box ${service.isOverlay ? 'overlay' : ''}`}
+                  style={{ backgroundImage: `url(${service.image})` }}
+                >
+                  <span >{service.name}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Home3;
